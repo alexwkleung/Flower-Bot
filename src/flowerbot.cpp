529c1034e79bcb77bfb252c6ff566ca18587dd25
Alex Leung <alexleung25@outlook.com>
2022-09-01 17:37:39 -0700

    Added commands

--- src/flowerbot.cpp
+++ src/flowerbot.cpp
@@ -28,7 +28,7 @@ void Bot::bot() {
     //create slash command event (/flower)
     bot.on_slashcommand([] (const dpp::slashcommand_t &event) {
         if(event.command.get_command_name() == "flowers") {
-            event.reply("Flowers are pretty and nice to look at!");
+            event.reply("Flowers are pretty and nice to look at! Fun fact: All the colours of all flowers can be combined to create a wonderful colour palette.");
         }
     });
 
@@ -36,8 +36,17 @@ void Bot::bot() {
     bot.on_slashcommand([] (const dpp::slashcommand_t &event) {
         if(event.command.get_command_name() == "iris") {
             event.reply(
-                "Iris is a genus of 260-300 species of flowering plants with showy flowers. It takes it name from the Greek word for a rainbow, which is also the name for the Greek goddess of the rainbow, Iris. You can read more at https://en.wikipedia.org/wiki/Iris_(plant)"
+                "Iris (genus Iris), genus of about 300 species of plants in the family Iriacaeae, including some of the world's most popular and varied garden flowers. The diversity of the genus is centered in the north temperate zone, though some of its most handsome species are native to the Mediterranean and central Asian areas. The iris is (arguably) the fleur-de-lis of the French royalist standard. It is a popular subject of Japanese flower arrangement, and it is also the source of orrisroot, from which 'essence of violet' perfume is made. Read more at https://www.britannica.com/plant/Iris-plant-genus" 
                 );
+        }
+    });
+
+    //create slash command event (/daisy)
+    bot.on_slashcommand([] (const dpp::slashcommand_t &event) {
+        if(event.command.get_command_name() == "daisy") {
+            event.reply(
+                "Daisy, any of several species of flowering plants belonging to the aster family (Asteraceae). The name daisy commonly denotes the oxeye daisy (Leucantheumum vulgare), the Shasta daisy (L. xsuperbum), and the English, or true, daisy (Bellis perennis). These and other plants called daisies are distinguished by a composite flower head composed of 15 to 30 white ray flowers surroudning a center consisting of bright yellow disk flowers, though other colour combinations are common. Read more at https://www.britannica.com/plant/daisy"
+            );
         }
     });
 
@@ -46,8 +55,9 @@ void Bot::bot() {
         //run registered commands once when bot is connected. prevents commands from being 
         //run repeatedly if bot reconnects.
         if(dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("Flowers", "Flowers?", bot.me.id));
-            bot.global_command_create(dpp::slashcommand("Iris", "Iris description.", bot.me.id));
+            bot.global_command_create(dpp::slashcommand("Flowers", "Flowers", bot.me.id));
+            bot.global_command_create(dpp::slashcommand("Iris", "Information about the Iris flower", bot.me.id));
+            bot.global_command_create(dpp::slashcommand("Daisy", "Information about the Daisy flower", bot.me.id));
         }
     });
 
